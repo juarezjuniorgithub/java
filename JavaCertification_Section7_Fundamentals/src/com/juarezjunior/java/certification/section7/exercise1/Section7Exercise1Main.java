d7a88e4fab924d12f5570e44d7d93c07419ed26e
Juarez Barbossa <Juarez Barbossa@192.168.192.6>
2017-04-19 20:42:10 +0100

    @juarezjuniorgithub

---
+++ JavaCertification_Section7_Fundamentals/src/com/juarezjunior/java/certification/section7/exercise1/Section7Exercise1Main.java
@@ -0,0 +1,30 @@
+package com.juarezjunior.java.certification.section7.exercise1;
+
+import static java.lang.Math.PI;
+import static java.awt.Color.*;
+
+/**
+ *
+ * @author juarezjuniorgithub
+ *
+ *         Section 7, Exercise 1
+ *
+ *         Create a class that demonstrates the use of static imports
+ *
+ *         References:
+ *
+ *         https://docs.oracle.com/javase/tutorial/java/package/usepkgs.html
+ *
+ */
+public class Section7Exercise1Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(7 * PI);
+		System.out.println(BLACK);
+		System.out.println(GREEN);
+		System.out.println(GRAY);
+
+	}
+
+}
